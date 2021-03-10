68bb47f7eb671db3399676aa94e8f0245ccaad9d
Brian Morton <bmorton925@gmail.com>
2021-03-10 18:04:52 -0500

    corrected part 2 start

---
+++ app.py
@@ -0,0 +1,36 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+##flask setup
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`"
+    )
+
+##test comment
